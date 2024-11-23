c51526a33023f0bc8b0685ee42a5a7e5cd7962ba
Piotr Matejek <devmatejekpior@gmail.com>
2024-11-23 08:06:13 +0100

    solved task03

---
+++ src/components/MD5Form.test.js
@@ -0,0 +1,45 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { getMd5 } from "./../providers/md5Provider";
+import Md5Form from "./Md5Form";
+
+describe("<Md5Form/>", () => {
+  it("input-value", async () => {
+    const text = "test";
+
+    render(<Md5Form />);
+    const input = await screen.findByRole("textbox");
+    userEvent.type(input, text);
+
+    const span = await screen.findByText(text);
+
+    expect(span).toBeInTheDocument();
+  });
+
+  it("input-strong", async () => {
+    const text = "test";
+    const md5 = "098f6bcd4621d373cade4e832627b4f6";
+    const spy = jest.spyOn(window, "fetch");
+
+    window.fetch.mockResolvedValue({
+      ok: true,
+      json: async () => {
+        return { Digest: md5 };
+      },
+    });
+
+    render(<Md5Form getMd5={getMd5} />);
+
+    const input = await screen.findByRole("textbox");
+    userEvent.type(input, text);
+
+    const button = await screen.findByRole("button");
+    userEvent.click(button);
+
+    await waitFor(async () => {
+      const strong = await screen.findByText(md5);
+      expect(strong).toBeInTheDocument();
+    });
+    spy.mockClear();
+  });
+});
